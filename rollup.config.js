64a1bad0f7b9566baed52655ff994504ad088535
darylsong <darylsong@tencent.com>
2022-11-29 12:00:37 +0800

    feat：简化代码

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,13 @@
+import { terser } from "rollup-plugin-terser";
+import cleanup from "rollup-plugin-cleanup";
 export default [
   {
     input: "./Wxml2Canvas/index.js",
+    plugins: [terser(), cleanup()],
     output: {
       file: "./dist.js",
       format: "es",
+      compact: true,
     },
     watch: {
       exclude: ["node_modules/**"],
